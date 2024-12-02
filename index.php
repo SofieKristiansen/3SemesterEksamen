2cc273357c0de8108df7084db00926c6417f41ec
emmao <emf002@edu.zealand.dk>
2024-12-02 19:03:17 +0100

    Fjernet indholdet

--- index.php
+++ index.php
@@ -1,31 +1,25 @@
-<?php
-require "settings/init.php";
-?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
 	<meta charset="utf-8">
 	
-	<title>Forsiden</title>
+	<title>Login - Klasse Challenge</title>
 	
 	<meta name="robots" content="All">
 	<meta name="author" content="Udgiver">
 	<meta name="copyright" content="Information om copyright">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Klima, Klasse, Konkurrence, Miljøvenlighed, Skole, ESG">
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-	
+
 
 </head>
-
 <body>
-<div>
 
-
-</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
